22ede608cc5aff288a9b4d462326a7450170ee7e
Kevin Cowley <cowley.kevin.l@gmail.com>
2016-10-18 22:48:08 -0600

    add clickAndHold, color selector

--- pixelJS.js
+++ pixelJS.js
@@ -10,13 +10,35 @@ for(let j=0;j<colorArray.length;j++){
       document.querySelector('#currentColor').style.background = colorChoice;
     });
 }
+//set selected color to variable colorChoice
+
+var colorPick = document.querySelector('#colorPicker');
+colorPick.addEventListener('click',function(event){
+  colorChoice = colorPick.value;
+  document.querySelector('#currentColor').style.background = colorChoice;
+});
+//set input color to brush color
 
 var pixel = document.getElementsByTagName('td');
 for(var i=0;i<pixel.length;i++){
-  pixel[i].addEventListener('click', function(event){
-  event.target.style.backgroundColor=colorChoice;
+  var bools = false;
+  document.querySelector('.grid').addEventListener('mousedown', function(){
+    bools=true;
   });
-}
+
+  pixel[i].addEventListener('mouseover', function(event){
+    if(bools){
+      event.target.style.backgroundColor=colorChoice;
+      console.log(bools);
+    }
+  });
+
+  document.querySelector('.grid').addEventListener('mouseup', function(){
+    bools=false;
+  });
+};
+//make clicked pixel have background color colorChoice
+//should the #grid event listeners be inside the for loop?
 
 var eraseAll = document.querySelector('#erase');
 eraseAll.addEventListener('click', function(event){
@@ -24,3 +46,4 @@ eraseAll.addEventListener('click', function(event){
     pixel[k].style.backgroundColor='white';
   }
 });
+//reset all pixels to white
